877f235c684154cfdb53646cc99f6d500b31d815
Arkadiusz Gawlik <arkadiusz_gawlik@epam.com>
2022-10-27 09:20:20 +0200

    small update

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,7 @@ namespace Task2
 {
     internal class Program
     {
+        static string digits = "0123456789ABCDEFGHIJK";
         static void Main(string[] args)
         {
             int original = int.Parse(args[0]);
@@ -23,36 +24,10 @@ namespace Task2
             StringBuilder converted = new();
             for(int i = 0; number > 0; i++)
             {
-                converted.Insert(0, ByteConversion(number % format));
+                converted.Insert(0, digits.ElementAt(number % format));
                 number /= format;
             }
             return converted.ToString();
-        }
-
-        static string ByteConversion(int number)
-        {
-            if(number <= 9)
-            {
-                return number.ToString();
-            }
-            else
-            {
-                switch (number)
-                {
-                    case 10: return "A";
-                        case 11: return "B";
-                        case 12: return "C";
-                        case 13: return "D";
-                        case 14: return "E";
-                        case 15: return "F";
-                        case 16: return "G";
-                        case 17: return "H";
-                        case 18: return "I";
-                        case 19: return "J";
-                    default:
-                        return "";
-                }
-            }
         }
     }
 }
